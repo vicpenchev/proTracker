dfb55f6dfaadfbe523c88bd81d82dc4e87db6bed
Viktor <vic.penchev@gmail.com>
2024-04-05 00:30:05 +0300

    Rules improvements

--- database/migrations/2024_03_23_135744_create_rule_field_rule_table.php
+++ database/migrations/2024_03_23_135744_create_rule_field_rule_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Rule;
+use App\Models\RuleField;
 
 return new class extends Migration
 {
@@ -11,12 +13,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rule_fields_pivot', function (Blueprint $table) {
+        Schema::create('rule_field_rule', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('rule_id')->nullable(false);
+            $table->foreignIdFor(RuleField::class);
+            $table->foreignIdFor(Rule::class);
+            /*$table->unsignedBigInteger('rule_id')->nullable(false);
             $table->foreign('rule_id')->references('id')->on('rules')->onDelete('cascade');
             $table->unsignedBigInteger('rule_field_id')->nullable(false);
-            $table->foreign('rule_field_id')->references('id')->on('rule_fields')->onDelete('cascade');
+            $table->foreign('rule_field_id')->references('id')->on('rule_fields')->onDelete('cascade');*/
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('rule_fields_pivot');
+        Schema::dropIfExists('rule_field_rule');
     }
 };
